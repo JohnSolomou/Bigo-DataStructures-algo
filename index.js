34cd56c7e12cbe05bebfeffa87cf53a342033947
John Solomou <solomoufam@yahoo.com>
2022-05-24 22:04:41 -0400

    adding linked list

--- index.js
+++ index.js
@@ -546,3 +546,58 @@ function firstRecurringCharacter2(input) {
   return undefined;
 } //O(n)
 console.log(firstRecurringCharacter2([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+
+//link lists
+//const basket = ['apples','grapes','pears'];
+//linked list: apples --> grapes --> pears
+
+//apples
+//4567 --> grapes
+//         7654  --> pears
+//                    435 -->null
+
+let obj1 = { a: true };
+let obj2 = obj1; //this is a pointer or a reference to another address in memory
+
+// 10-->5-->16
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {}
+}
+
+const myLinkedList = new LinkedList(10);
+// myLinkedList.append(5);
+// myLinkedList.append(16);
+// console.log(myLinkedList);
+console.log(myLinkedList.append(5));
